5a262dfebd5e45602857dae20479732defd86692
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-21 15:31:24 +0100

    Modified nyc_taxi_analysis.py. Added functions

--- nyc_taxi_analysis.py
+++ nyc_taxi_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-
 from geopy import Nominatim
+from datetime import datetime
 
 pd.set_option("display.max_columns", 30)
 pd.set_option("display.width", 1000)
@@ -13,6 +13,7 @@ class NYCTaxiAnalysis:
         self.data = (
             pd.read_csv(dataset)
             .query("@date_from <= pickup_datetime <= @date_to")
+            .sort_values(by='pickup_datetime')
             .head(lines)
         )
 
@@ -29,6 +30,9 @@ class NYCTaxiAnalysis:
         self.dropoff_address = None
 
     def rides_of_the_day(self):
+        """
+        1. Analyze taxi rides based on time of day.
+        """
         # This function analyze taxi rides based on time of day: morning, afternoon, evening, late night/early morning
         try:
             # Convert pickup_datetime column to datetime dtypes
@@ -38,25 +42,26 @@ class NYCTaxiAnalysis:
             self.morning_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("06:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("12:00:00").time())
-            ].sort_values(by="pickup_datetime", ascending=True)
+                ].sort_values(by="pickup_datetime", ascending=True)
 
             self.afternoon_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("12:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("18:00:00").time())
-            ].sort_values(by="pickup_datetime", ascending=True)
+                ].sort_values(by="pickup_datetime", ascending=True)
 
             self.evening_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("18:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("23:59:59").time())
-            ].sort_values(by="pickup_datetime", ascending=True)
+                ].sort_values(by="pickup_datetime", ascending=True)
 
             self.night_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("00:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("06:00:00").time())
-            ].sort_values(by="pickup_datetime", ascending=True)
+                ].sort_values(by="pickup_datetime", ascending=True)
 
             return (
-                f"For the period {self.date_from} through {self.date_to} there were:\n"
+                f"For the period {datetime.strptime(self.date_from, '%Y-%m-%d').strftime('%B %d, %Y')}"
+                f" through {datetime.strptime(self.date_to, '%Y-%m-%d').strftime('%B %d, %Y')} there were:\n"
                 f"\t{len(self.morning_rides)} rides from 6:00 a.m. (06:00:00) to 12:00 p.m. (12:00:00);\n"
                 f"\t{len(self.afternoon_rides)} rides from 12:00 p.m. (12:00:00) to 6:00 p.m. (18:00:00);\n"
                 f"\t{len(self.evening_rides)} rides from 6:00 p.m. (18:00:00) to 12 a.m. (00:00:00);\n"
@@ -115,21 +120,67 @@ class NYCTaxiAnalysis:
             for coord in self.dropoff_coordinates
         ]
 
-        return self.data.sort_values(by="pickup_datetime", ascending=True)
+        # Create new csv file with proceeded data and added addresses
+        self.data.to_csv('CSV/nyc_taxi_trips_addresses.csv', index=False)
+
+    def most_common_locations(self):
+        """
+        2. Identify popular pickup and drop-off locations.
+        """
+        # This function count number of locations.
+        pickup_bronx_number = self.data[self.data['pickup_borough'] == 'Bronx'].__len__()
+        dropoff_bronx_number = self.data[self.data['dropoff_borough'] == 'Bronx'].__len__()
+
+        pickup_brooklyn_number = self.data[self.data['pickup_borough'] == 'Brooklyn'].__len__()
+        dropoff_brooklyn_number = self.data[self.data['dropoff_borough'] == 'Brooklyn'].__len__()
+
+        pickup_manhattan_number = self.data[self.data['pickup_borough'] == 'Manhattan'].__len__()
+        dropoff_manhattan_number = self.data[self.data['dropoff_borough'] == 'Manhattan'].__len__()
+
+        pickup_queens_number = self.data[self.data['pickup_borough'] == 'Queens'].__len__()
+        dropoff_queens_number = self.data[self.data['dropoff_borough'] == 'Queens'].__len__()
+
+        pickup_staten_island_number = self.data[self.data['pickup_borough'] == 'Staten Island'].__len__()
+        dropoff_staten_island_number = self.data[self.data['dropoff_borough'] == 'Staten Island'].__len__()
+
+        return (
+            f"For the period {datetime.strptime(self.date_from, '%Y-%m-%d').strftime('%B %d, %Y')}"
+            f" through {datetime.strptime(self.date_to, '%Y-%m-%d').strftime('%B %d, %Y')} there were:\n"
+            f"\tIn Bronx, there were {pickup_bronx_number} of passenger pickups "
+            f"and {dropoff_bronx_number} drop-offs;\n"
+            f"\tIn Brooklyn, there were {pickup_brooklyn_number} passenger pickups "
+            f"and {dropoff_brooklyn_number} drop-offs;\n"
+            f"\tIn Manhattan, there were {pickup_manhattan_number} passenger pickups "
+            f"and {dropoff_manhattan_number} drop-offs;\n"
+            f"\tIn Queens, there were {pickup_queens_number} passenger pickups "
+            f"and {dropoff_queens_number} drop-offs;\n"
+            f"\tIn Staten Island, there were {pickup_staten_island_number} passenger pickups "
+            f"and {dropoff_staten_island_number} drop-offs."
+        )
+
+    def weather_by_day(self):
+        """
+        3. Analyze the impact of weather on taxi rides.
+        """
+        weather_data = pd.read_csv("CSV/nyc_weather_central_park_2016.csv")
+        return weather_data
 
     def analysis_visualization(self):
-        self.pickup_dropoff_addresses()
+        # self.pickup_dropoff_addresses()
+        new_data = pd.read_csv('CSV/nyc_taxi_trips_addresses.csv')
+        return new_data
 
 
 if __name__ == "__main__":
     nyc_taxi_analysis = NYCTaxiAnalysis(
         dataset="CSV/nyc_taxi_trips.csv",
         date_from="2016-01-01",
-        date_to="2016-01-30",
-        lines=10,
+        date_to="2016-01-31",
+        # lines=2,
     )
 
     print(nyc_taxi_analysis.rides_of_the_day())
-    print(nyc_taxi_analysis.pickup_dropoff_addresses())
+    # print(nyc_taxi_analysis.pickup_dropoff_addresses())
+    # print(nyc_taxi_analysis.weather_by_day())
 
     # print(nyc_taxi_analysis.analysis_visualization())
