bcee41e5cb17b6fad79f54acc48267a085910877
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-25 18:36:41 +0100

    Replaced function pickup_dropoff_adresses to handler.py

--- nyc_taxi_analysis.py
+++ nyc_taxi_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
-from geopy import Nominatim
+
 from datetime import datetime
+
 
 pd.set_option("display.max_columns", 30)
 pd.set_option("display.width", 1000)
@@ -24,11 +24,6 @@ class NYCTaxiAnalysis:
         self.date_from = date_from
         self.date_to = date_to
 
-        self.pickup_coordinates = None
-        self.dropoff_coordinates = None
-        self.pickup_address = None
-        self.dropoff_address = None
-
     def rides_of_the_day(self):
         """
         1. Analyze taxi rides based on time of day.
@@ -42,22 +37,22 @@ class NYCTaxiAnalysis:
             self.morning_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("06:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("12:00:00").time())
-                ].sort_values(by="pickup_datetime", ascending=True)
+                ]
 
             self.afternoon_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("12:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("18:00:00").time())
-                ].sort_values(by="pickup_datetime", ascending=True)
+                ]
 
             self.evening_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("18:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("23:59:59").time())
-                ].sort_values(by="pickup_datetime", ascending=True)
+                ]
 
             self.night_rides = self.data.loc[
                 (self.data["pickup_datetime"].dt.time >= pd.to_datetime("00:00:00").time())
                 & (self.data["pickup_datetime"].dt.time <= pd.to_datetime("06:00:00").time())
-                ].sort_values(by="pickup_datetime", ascending=True)
+                ]
 
             return (
                 f"For the period {datetime.strptime(self.date_from, '%Y-%m-%d').strftime('%B %d, %Y')}"
@@ -72,56 +67,6 @@ class NYCTaxiAnalysis:
         except KeyError as err:
             # Display error message in error key
             return f"WARNING! The function {NYCTaxiAnalysis.rides_of_the_day.__name__}() error - {err}"
-
-    def pickup_dropoff_addresses(self):
-        # This function get all pickup and dropoff addresses by longitude/latitude and add it to dataframe
-        pickup_lat = self.data["pickup_latitude"].values
-        pickup_lon = self.data["pickup_longitude"].values
-        dropoff_lat = self.data["dropoff_latitude"].values
-        dropoff_lon = self.data["dropoff_longitude"].values
-
-        # Stack the latitude/longitude arrays horizontally and transpose to create tuples
-        self.pickup_coordinates = np.vstack((pickup_lat, pickup_lon)).T
-        self.dropoff_coordinates = np.vstack((dropoff_lat, dropoff_lon)).T
-
-        geolocator = Nominatim(user_agent="NYC_taxi")
-
-        # Get exact pickup address, street and borough by location
-        self.data["pickup_address"] = [
-            geolocator.reverse(coord).address for coord in self.pickup_coordinates
-        ]
-        # Instead (try, except KeyError) use .get('road', None) method
-        # in case 'road' key is missing in address else None
-        self.data["pickup_street"] = [
-            geolocator.reverse(coord).raw["address"].get("road", None)
-            for coord in self.pickup_coordinates
-        ]
-        # Instead (try, except KeyError) use .get('suburb', None) method
-        # in case 'suburb' key is missing in address else None
-        self.data["pickup_borough"] = [
-            geolocator.reverse(coord).raw["address"].get("suburb", None)
-            for coord in self.pickup_coordinates
-        ]
-
-        # Get exact pickup address, street and borough by location
-        self.data["dropoff_address"] = [
-            geolocator.reverse(coord).address for coord in self.dropoff_coordinates
-        ]
-        # Instead (try, except KeyError) use .get('road', None) method
-        # in case 'road' key is missing in address else None
-        self.data["dropoff_street"] = [
-            geolocator.reverse(coord).raw["address"].get("road", None)
-            for coord in self.dropoff_coordinates
-        ]
-        # Instead (try, except KeyError) use .get('suburb', None) method
-        # in case 'suburb' key is missing in address else None
-        self.data["dropoff_borough"] = [
-            geolocator.reverse(coord).raw["address"].get("suburb", None)
-            for coord in self.dropoff_coordinates
-        ]
-
-        # Create new csv file with proceeded data and added addresses
-        self.data.to_csv('CSV/nyc_taxi_trips_addresses.csv', index=False)
 
     def most_common_locations(self):
         """
@@ -162,25 +107,25 @@ class NYCTaxiAnalysis:
         """
         3. Analyze the impact of weather on taxi rides.
         """
-        weather_data = pd.read_csv("CSV/nyc_weather_central_park_2016.csv")
+        weather_data = pd.read_csv("CSV/datasets/nyc_weather_central_park_2016.csv")
         return weather_data
 
     def analysis_visualization(self):
         # self.pickup_dropoff_addresses()
-        new_data = pd.read_csv('CSV/nyc_taxi_trips_addresses.csv')
-        return new_data
+        new_data = pd.read_csv('CSV/nyc_taxi.csv').head(1)
+        return new_data.loc[0, 'pickup_longitude'], new_data.loc[0, 'pickup_latitude']
 
 
 if __name__ == "__main__":
     nyc_taxi_analysis = NYCTaxiAnalysis(
-        dataset="CSV/nyc_taxi_trips.csv",
+        dataset="CSV/datasets/nyc_taxi_trips.csv",
         date_from="2016-01-01",
-        date_to="2016-01-31",
+        date_to="2016-12-31",
         # lines=2,
     )
 
-    print(nyc_taxi_analysis.rides_of_the_day())
+    # print(nyc_taxi_analysis.rides_of_the_day())
     # print(nyc_taxi_analysis.pickup_dropoff_addresses())
     # print(nyc_taxi_analysis.weather_by_day())
 
-    # print(nyc_taxi_analysis.analysis_visualization())
+    print(nyc_taxi_analysis.analysis_visualization())
