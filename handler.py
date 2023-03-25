965b888a3c306273295c515c7352a43cb69756f9
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-25 18:35:30 +0100

    Replaced function pickup_dropoff_adresses to handler.py

--- handler.py
+++ handler.py
@@ -1,23 +1,43 @@
 import pandas as pd
 import numpy as np
+
 from geopy.geocoders import Nominatim
-from datetime import datetime
+from functools import lru_cache
+
+
+# Create geolocator object outside the function, so it can be reused
+GEOLOCATOR = Nominatim(user_agent="NYC_taxi")
+
+
+# Use LRU cache to cache the most recently addresses
+@lru_cache(maxsize=1000)
+def get_location(coord):
+    # Convert NumPy array to tuple
+    coord = tuple(coord)
+
+    # Check cache for address before calling geolocator
+    if coord in get_location.cache:
+        location = get_location.cache[coord]
+    else:
+        location = GEOLOCATOR.reverse(coord)
+        get_location.cache[coord] = location
+    return location
+
+
+get_location.cache = {}
 
 
 def pickup_dropoff_addresses(filename, batch_size):
     # This function get all pickup and dropoff addresses by longitude/latitude and add it to dataframe
-    data = pd.read_csv(filename).sort_values(by="pickup_datetime")
-
-    pickup_lat = data["pickup_latitude"].values
-    pickup_lon = data["pickup_longitude"].values
-    dropoff_lat = data["dropoff_latitude"].values
-    dropoff_lon = data["dropoff_longitude"].values
+    data = pd.read_csv(filename).sort_values(by="pickup_datetime").head(5)
 
     # Stack the latitude/longitude arrays horizontally and transpose to create tuples
-    pickup_coordinates = np.vstack((pickup_lat, pickup_lon)).T
-    dropoff_coordinates = np.vstack((dropoff_lat, dropoff_lon)).T
-
-    geolocator = Nominatim(user_agent="NYC_taxi")
+    pickup_coordinates = np.vstack(
+        (data["pickup_latitude"].values, data["pickup_longitude"].values)
+    ).T
+    dropoff_coordinates = np.vstack(
+        (data["dropoff_latitude"].values, data["dropoff_longitude"].values)
+    ).T
 
     pickup_address = []
     pickup_street = []
@@ -27,11 +47,9 @@ def pickup_dropoff_addresses(filename, batch_size):
     dropoff_street = []
     dropoff_borough = []
 
-    # Set cache dict
-    cache = {}
-
-    # Get exact pickup address, street and borough by location
     counter_ = 0
+
+    # Use list comprehension for faster and more concise code
     for batch_idx in range(0, len(pickup_coordinates), batch_size):
         pickup_coords_batch = pickup_coordinates[batch_idx: batch_idx + batch_size]
         dropoff_coords_batch = dropoff_coordinates[batch_idx: batch_idx + batch_size]
@@ -40,17 +58,13 @@ def pickup_dropoff_addresses(filename, batch_size):
             (pickup_coords_batch, pickup_address, pickup_street, pickup_borough),
             (dropoff_coords_batch, dropoff_address, dropoff_street, dropoff_borough),
         ]:
-
             for coord in coords:
                 counter_ += 1
-                print(f"{datetime.now().time().strftime('%H:%M:%S')}, {counter_} line processed...")
-                # Check cache for address before calling geolocator
-                if tuple(coord) in cache:
-                    location = cache[tuple(coord)]
-                else:
-                    location = geolocator.reverse(coord)
-                    cache[tuple(coord)] = location
 
+                # Get location from cache or geolocator
+                location = get_location(tuple(coord))
+
+                # Append data to respective lists
                 addr_list.append(location.address)
                 street_list.append(location.raw["address"].get("road", None))
                 borough_list.append(location.raw["address"].get("suburb", None))
@@ -64,10 +78,8 @@ def pickup_dropoff_addresses(filename, batch_size):
     data["dropoff_borough"] = dropoff_borough
 
     # Create new csv file with proceeded data and added addresses
-    print(f"\n{datetime.now().time().strftime('%H:%M:%S')}, {counter_} lines processed")
-
-    data.to_csv("CSV/nyc_taxi_trips_addresses.csv")
-    print("Successfully created file with addresses 'CSV/nyc_taxi_trips_addresses.csv'")
+    data.to_csv("CSV/nyc_taxi.csv")
+    print("Successfully created file with addresses 'CSV/nyc_taxi.csv'")
 
 
-pickup_dropoff_addresses("CSV/nyc_taxi_trips.csv", batch_size=100)
+pickup_dropoff_addresses("CSV/datasets/nyc_taxi_trips.csv", batch_size=500)
