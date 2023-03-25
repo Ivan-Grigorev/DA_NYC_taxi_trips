3f558454e7222ff22492495c56219493ec74d003
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-03-25 18:37:26 +0100

    Added tests for handler.py

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,31 +7,31 @@ from handler import pickup_dropoff_addresses
 class TestPickupDropoffAddresses(unittest.TestCase):
     def test_pickup_dropoff_addresses(self):
         # Ensure the pickup and dropoff addresses are correctly retrieved
-        addresses = pickup_dropoff_addresses('CSV/test_data.csv')
+        addresses = pickup_dropoff_addresses('CSV/test_data.csv', 100)
 
-        # self.assertIsNotNone(self.analysis.pickup_coordinates)
-        # self.assertIsNotNone(self.analysis.dropoff_coordinates)
+        # self.assertIsNotNone(pickup_coordinates)
+        # self.assertIsNotNone(dropoff_coordinates)
 
-        # self.assertEqual(
-        #     addresses.loc[0, "pickup_address"],
-        #     "Spectrum, 261, 3rd Avenue, Manhattan Community Board 6, "
-        #     "Manhattan, New York County, City of New York, New York, 10010, United States",
-        # )
-        # self.assertEqual(
-        #     addresses.loc[0, "dropoff_address"],
-        #     "Pennsylvania Station, Lincoln Tunnel Expressway, Chelsea District, "
-        #     "Manhattan, New York County, City of New York, New York, 10119, United States",
-        # )
-        # self.assertEqual(
-        #     addresses.loc[2, "pickup_address"],
-        #     "251, Church Street, Manhattan Community Board 1, "
-        #     "Manhattan, New York County, City of New York, New York, 10013, United States",
-        # )
-        # self.assertEqual(
-        #     addresses.loc[2, "dropoff_address"],
-        #     "258, West 4th Street, Manhattan Community Board 2, "
-        #     "Manhattan, New York County, City of New York, New York, 10014, United States",
-        # )
+        self.assertEqual(
+            addresses.loc[0, "pickup_address"],
+            "Spectrum, 261, 3rd Avenue, Manhattan Community Board 6, "
+            "Manhattan, New York County, City of New York, New York, 10010, United States",
+        )
+        self.assertEqual(
+            addresses.loc[0, "dropoff_address"],
+            "Pennsylvania Station, Lincoln Tunnel Expressway, Chelsea District, "
+            "Manhattan, New York County, City of New York, New York, 10119, United States",
+        )
+        self.assertEqual(
+            addresses.loc[2, "pickup_address"],
+            "251, Church Street, Manhattan Community Board 1, "
+            "Manhattan, New York County, City of New York, New York, 10013, United States",
+        )
+        self.assertEqual(
+            addresses.loc[2, "dropoff_address"],
+            "258, West 4th Street, Manhattan Community Board 2, "
+            "Manhattan, New York County, City of New York, New York, 10014, United States",
+        )
 
 
 if __name__ == "__main__":
