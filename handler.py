7c9dc3aff7d60874c887ab4810d862e46274a376
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-04-02 11:33:21 +0200

    pickup_dropoff_adresses() get addresses using Nominatim and OpenStreetMap

--- handler.py
+++ handler.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np
+import requests
+import time
+
+from geopy import Nominatim
 
 
-pd.set_option('display.max_columns', 50)
-pd.set_option('display.width', 1000)
+pd.set_option("display.max_columns", None)
+pd.set_option('display.max_rows', None)
+pd.set_option("display.width", 1000)
+pd.set_option('max_colwidth', 100)
 
 
 class HandleDatasets:
@@ -13,19 +20,68 @@ class HandleDatasets:
         self.data = data
 
     def pickup_dropoff_addresses(self):
-        for index, row in self.addresses.iterrows():
-            string_ = row['the_geom'].replace('MULTILINESTRING ((', '').replace('))', '')
-            self.addresses.loc[index, 'the_geom'] = string_
-        self.addresses.to_csv('CSV/datasets/nyc_streets.csv', index=False)
+        start_time = time.time()
+        # Get pickup and dropoff coordinates
+        pickup_coordinates = [f"{_[0]} {_[1]}" for _ in np.column_stack(
+            (self.trips['pickup_latitude'].values, self.trips['pickup_longitude'].values))
+                              ]
+
+        dropoff_coordinates = [f"lat={_[0]}&lon={_[1]}" for _ in np.column_stack(
+            (self.trips['dropoff_latitude'].values, self.trips['dropoff_longitude'].values))
+                               ]
+
+        # Get streets names, boroughs by pickup and dropoff coordinates
+        # Using geopy and Nominatim
+        GEOLOCATOR = Nominatim(user_agent='NYC_taxi')
+
+        self.trips['pickup_address'] = [
+            GEOLOCATOR.reverse(coord).address
+            for coord in pickup_coordinates
+        ]
+        self.trips['pickup_street'] = [
+            GEOLOCATOR.reverse(coord).raw['address'].get('road', None)
+            for coord in pickup_coordinates
+        ]
+        self.trips['pickup_borough'] = [
+            GEOLOCATOR.reverse(coord).raw['address'].get('suburb', None)
+            for coord in pickup_coordinates
+        ]
+
+        # Using requests and OpenStreetMap
+        self.trips['dropoff_address'] = [
+            requests.get(
+                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
+            ).json().get('display_name', {})
+            for coord in dropoff_coordinates
+        ]
+        self.trips['dropoff_street'] = [
+            requests.get(
+                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
+            ).json().get('address', {}).get('road', None)
+            for coord in dropoff_coordinates
+        ]
+        self.trips['dropoff_borough'] = [
+            requests.get(
+                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
+            ).json().get('address', {}).get('suburb', None)
+            for coord in dropoff_coordinates
+        ]
+
+        print(f"The {HandleDatasets.pickup_dropoff_addresses.__name__}() function successfully completed "
+              f"processing {len(self.trips)} lines in {round((time.time() - start_time), 2)} seconds.")
+
+    def __repr__(self):
+        return self.trips
 
 
 if __name__ == '__main__':
     handle_datasets = HandleDatasets(
         trips_file='CSV/datasets/nyc_taxi_trips.csv',
-        addresses_file='CSV/datasets/Centerline.csv',
+        addresses_file='CSV/datasets/nyc_streets.csv',
         weather_file='CSV/datasets/nyc_weather_central_park.csv',
         data='CSV/nyc_taxi.csv',
-        # lines=5
+        lines=5,
     )
 
     handle_datasets.pickup_dropoff_addresses()
+    print(handle_datasets.__repr__())
