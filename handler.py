b43f6f1c07ea5084efc3c4b2a672b215f20f2637
Ivan Grigorev <iv.st.grigorev@gmail.com>
2023-04-08 20:37:31 +0200

    pickup_dropoff_address() get addresses usin multiprocessing

--- handler.py
+++ handler.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import requests
 import time
+import multiprocessing
 
-from geopy import Nominatim
-
+from geopy.geocoders import Nominatim
 
 pd.set_option("display.max_columns", None)
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_rows", None)
 pd.set_option("display.width", 1000)
-pd.set_option('max_colwidth', 100)
+pd.set_option("max_colwidth", 100)
 
 
 class HandleDatasets:
@@ -19,68 +19,66 @@ class HandleDatasets:
         self.weather = pd.read_csv(weather_file, low_memory=False).head(lines)
         self.data = data
 
+    # Get streets names, boroughs by pickup and dropoff coordinates
+    def get_pickup_location(self, coord):
+        # Using geopy and Nominatim
+        geolocator = Nominatim(user_agent="NYC_taxi", timeout=5)
+        return geolocator.reverse(coord)
+
+    def get_dropoff_location(self, coord):
+        # Using requests and OpenStreetMap
+        return requests.get(
+            f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
+        ).json()
+
     def pickup_dropoff_addresses(self):
         start_time = time.time()
         # Get pickup and dropoff coordinates
-        pickup_coordinates = [f"{_[0]} {_[1]}" for _ in np.column_stack(
-            (self.trips['pickup_latitude'].values, self.trips['pickup_longitude'].values))
-                              ]
-
-        dropoff_coordinates = [f"lat={_[0]}&lon={_[1]}" for _ in np.column_stack(
-            (self.trips['dropoff_latitude'].values, self.trips['dropoff_longitude'].values))
-                               ]
-
-        # Get streets names, boroughs by pickup and dropoff coordinates
-        # Using geopy and Nominatim
-        GEOLOCATOR = Nominatim(user_agent='NYC_taxi')
-
-        self.trips['pickup_address'] = [
-            GEOLOCATOR.reverse(coord).address
-            for coord in pickup_coordinates
-        ]
-        self.trips['pickup_street'] = [
-            GEOLOCATOR.reverse(coord).raw['address'].get('road', None)
-            for coord in pickup_coordinates
-        ]
-        self.trips['pickup_borough'] = [
-            GEOLOCATOR.reverse(coord).raw['address'].get('suburb', None)
-            for coord in pickup_coordinates
+        pickup_coordinates = [
+            f"{_[0]} {_[1]}" for _ in np.column_stack((
+                    self.trips["pickup_latitude"].values,
+                    self.trips["pickup_longitude"].values,))
         ]
 
-        # Using requests and OpenStreetMap
-        self.trips['dropoff_address'] = [
-            requests.get(
-                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
-            ).json().get('display_name', {})
-            for coord in dropoff_coordinates
-        ]
-        self.trips['dropoff_street'] = [
-            requests.get(
-                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
-            ).json().get('address', {}).get('road', None)
-            for coord in dropoff_coordinates
-        ]
-        self.trips['dropoff_borough'] = [
-            requests.get(
-                f"https://nominatim.openstreetmap.org/reverse?{coord}&format=json"
-            ).json().get('address', {}).get('suburb', None)
-            for coord in dropoff_coordinates
+        dropoff_coordinates = [
+            f"lat={_[0]}&lon={_[1]}" for _ in np.column_stack((
+                    self.trips["dropoff_latitude"].values,
+                    self.trips["dropoff_longitude"].values,))
         ]
 
-        print(f"The {HandleDatasets.pickup_dropoff_addresses.__name__}() function successfully completed "
-              f"processing {len(self.trips)} lines in {round((time.time() - start_time), 2)} seconds.")
+        # Use multiprocessing to speed up geocoding
+        pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+
+        pickup_data = pool.map(self.get_pickup_location, pickup_coordinates)
+        dropoff_data = pool.map(self.get_dropoff_location, dropoff_coordinates)
+
+        self.trips["pickup_address"] = [i.address for i in pickup_data]
+        self.trips["pickup_street"] = [i.raw["address"].get("road", None) for i in pickup_data]
+        self.trips["pickup_borough"] = [i.raw["address"].get("suburb", None) for i in pickup_data]
+
+        self.trips["dropoff_address"] = [i.get("display_name", {}) for i in dropoff_data]
+        self.trips["dropoff_street"] = [i.get("address", {}).get("road", {}) for i in dropoff_data]
+        self.trips["dropoff_borough"] = [i.get("address", {}).get("suburb", {}) for i in dropoff_data]
+
+        # Create new dataset with processed data
+        self.trips.to_csv(self.data, index=False)
+
+        print(
+            f"The {HandleDatasets.pickup_dropoff_addresses.__name__}() function successfully completed "
+            f"processing {len(self.trips)} lines in {round((time.time() - start_time), 2)} seconds."
+        )
 
     def __repr__(self):
-        return self.trips
+        return pd.read_csv(self.data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     handle_datasets = HandleDatasets(
-        trips_file='CSV/datasets/nyc_taxi_trips.csv',
-        addresses_file='CSV/datasets/nyc_streets.csv',
-        weather_file='CSV/datasets/nyc_weather_central_park.csv',
-        data='CSV/nyc_taxi.csv',
-        lines=5,
+        trips_file="CSV/datasets/nyc_taxi_trips.csv",
+        addresses_file="CSV/datasets/nyc_streets.csv",
+        weather_file="CSV/datasets/nyc_weather_central_park.csv",
+        data="CSV/nyc_taxi.csv",
+        lines=20,
     )
 
     handle_datasets.pickup_dropoff_addresses()
